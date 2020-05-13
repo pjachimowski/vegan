5231530e9b37cb139cd537ee6575083030add149
pjachimowski <p.jachimowski89@gmail.com>
2020-05-13 15:22:45 +0000

    setting up jquery and materialised and wireing up to mongo DB

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'vegan'
+app.config["MONGO_URI"]='mongodb+srv://root:r00tUser@myfirstcluster-xvp8g.mongodb.net/vegan?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hell():
-    return "Hello World"
+@app.route('/get_vegan_day')
+def get_vegan_day():
+    return render_template("vegan_day.html", 
+    recipes=mongo.db.recipes.find())
+
+
 
 
 if __name__ == '__main__':
