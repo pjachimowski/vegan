868c26362b5382b68c8f0655d532c59ba34b4dea
pjachimowski <p.jachimowski89@gmail.com>
2020-05-13 19:56:57 +0000

    Added edit_recipe.html working on EDIT and DELETE buttons

--- app.py
+++ app.py
@@ -15,14 +15,26 @@ mongo = PyMongo(app)
 @app.route('/get_vegan_day')
 def get_vegan_day():
     return render_template("vegan_day.html", 
-    recipes=mongo.db.recipes.find())
+                        recipes = mongo.db.recipes.find())
 
 
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("add_recipe.html", 
-    recipes=mongo.db.recipes.find())
+                            categories=mongo.db.categories.find())
 
+                            
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes=mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_vegan_day'))
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edit_recipe.html', recipe=the_recipe, categories=all_categories)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
